69cf95870240dcff91b2f74f9cc04d823e6362c7
Matt Borowiec <mborowiec@gmail.com>
2018-05-05 11:57:15 -0700

    Excercise 1.3

--- ch1/excercises/1.3/main_test.go
+++ ch1/excercises/1.3/main_test.go
@@ -7,17 +7,49 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
-func Echo3(s ...string) {
-	fmt.Println(strings.Join(os.Args[1:], " ", s))
+func Echo1(str ...string) string {
+	var s, sep string
+	for i := 0; i < len(str); i++ {
+		s += sep + str[i]
+		sep = " "
+	}
+	return s
+}
+
+func Echo2(str ...string) string {
+	s, sep := "", ""
+	for _, arg := range str {
+		s += sep + arg
+		sep = " "
+	}
+	return s
+}
+
+func Echo3(s ...string) string {
+	return strings.Join(s, " ")
 }
 
 //!+
+
+func BenchmarkEcho1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Echo1("echo1", "arg0", "arg1")
+	}
+}
+
+func BenchmarkEcho2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Echo2("echo2", "arg0", "arg1")
+	}
+}
+
 func BenchmarkEcho3(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Echo3("k", "g", 3)
+		Echo3("echo3", "arg0", "arg1")
 	}
 }
 
